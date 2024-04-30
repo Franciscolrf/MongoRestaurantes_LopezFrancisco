57a387b919968ea45a48fe0a1b06bfd5e9e9107e
Fran <francisco.lopez247037@potros.itson.edu.mx>
2024-04-29 23:57:44 -0700

    clase restaurantes

---
+++ restaurantesMongo/src/main/java/bda/itson/restaurantesmongo/RestaurantesMongo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bda.itson.restaurantesmongo;
+
+/**
+ *
+ * @author ID145
+ */
+public class RestaurantesMongo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ restaurantesMongo/src/main/java/modelos/Restaurantes.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+import java.util.Date;
+
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author ID145
+ */
+public class Restaurantes {
+    private ObjectId id;
+    private String nombre;
+    private int rating;
+    private Date fecha;
+    private String categorias[];
+
+    public Restaurantes() {
+    }
+
+    public Restaurantes(ObjectId id, String nombre, int rating, Date fecha, String categorias[]) {
+        this.id = id;
+        this.nombre = nombre;
+        this.rating = rating;
+        this.fecha = fecha;
+        this.categorias = categorias;
+
+    }
+
+    public Restaurantes(String nombre, int rating, Date fecha, String categorias[]) {
+        this.nombre = nombre;
+        this.rating = rating;
+        this.fecha = fecha;
+        this.categorias = categorias;
+
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String[] getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(String[] categorias) {
+        this.categorias = categorias;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurantes{" + "id=" + id + ", nombre=" + nombre + ", rating=" + rating + ", fecha=" + fecha + ", categorias=" + categorias + '}';
+    }
+
+}
